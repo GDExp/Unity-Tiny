0287ed198cf72d4200f17a3d189664c8e3163f89
GDExp <40066886+GDExp@users.noreply.github.com>
2019-02-16 14:20:24 +0300

    Add files via upload (#4)

---
+++ development/artifacts/bindings/bind-generated.cs
@@ -0,0 +1,348 @@
+using Unity.Collections.LowLevel.Unsafe;
+namespace entities.game.BlocksGroup
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Component : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace entities.game.MainScene
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Component : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Block : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Ground : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct InputSystem : UTiny.IComponentData
+    {
+
+
+        public float jumpForce;
+        public bool grounded;
+        public bool hit;
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct MoveBlock : UTiny.IComponentData
+    {
+
+
+        public float blockSpeed;
+        public bool move;
+        public bool setup;
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Player : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Score : UTiny.IComponentData
+    {
+
+
+        public int Scores;
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Scroller : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace game
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Spawner : UTiny.IComponentData
+    {
+
+
+        public bool isPaused;
+        public float plusSpeed;
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Default : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct TransparentFX : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct IgnoreRaycast : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Water : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct UI : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct PostProcessing : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.Core2D.layers
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct Cutscene : UTiny.IComponentData
+    {
+
+
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceAnimationClip : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceAudioClip : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceSprite : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceSpriteAtlas : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceTexture2D : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceTileBase : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct AssetReferenceTMP_FontAsset : UTiny.IComponentData
+    {
+
+
+        public string guid;
+        public long fileId;
+        public int type;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct CameraCullingMask : UTiny.IComponentData
+    {
+
+
+        public int mask;
+
+
+
+    }
+}
+namespace ut.EditorExtensions
+{
+    [global::System.Runtime.InteropServices.StructLayout(global::System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public struct EntityLayer : UTiny.IComponentData
+    {
+
+
+        public int layer;
+
+
+
+    }
+}
+
+
+
---
+++ development/build/main.js
@@ -0,0 +1,91 @@
+﻿/**
+ * TINY GENERATED CODE, DO NOT EDIT BY HAND
+ * @project Unity Tiny - Stack Jump
+ */
+
+console.log('runtime version: internal');
+
+ut.importModule(ut.Core2D);
+ut.importModule(ut.Math);
+ut.importModule(ut);
+ut.importModule(ut.Shared);
+ut.importModule(ut.Core2D);
+ut.importModule(ut.HTML);
+ut.importModule(ut.Rendering);
+ut.importModule(ut.Rendering);
+ut.importModule(ut.HTML);
+ut.importModule(ut.Core2D);
+ut.importModule(ut.Rendering);
+ut.importModule(ut.Rendering);
+ut.importModule(ut.Core2D);
+ut.importModule(ut.Physics2D);
+ut.importModule(ut.HitBox2D);
+ut.importModule(ut.UILayout);
+ut.importModule(ut.Text);
+ut.importModule(ut.HTML);
+ut.main = function() {
+    // Singleton world
+    var world = new ut.World();
+
+    // Schedule all systems
+    var scheduler = world.scheduler();
+    game.BlockMovementJS.update = new game.BlockMovement()._MakeSystemFn();
+    game.PlayerCollisionSystemJS.update = new game.PlayerCollisionSystem()._MakeSystemFn();
+    game.PlayerInputJS.update = new game.PlayerInput()._MakeSystemFn();
+    game.ScoreSystemJS.update = new game.ScoreSystem()._MakeSystemFn();
+    game.SpawnerSystemsJS.update = new game.SpawnerSystems()._MakeSystemFn();
+    scheduler.schedule(game.BlockMovementJS);
+    scheduler.schedule(game.PlayerCollisionSystemJS);
+    scheduler.schedule(game.PlayerInputJS);
+    scheduler.schedule(game.ScoreSystemJS);
+    scheduler.schedule(game.SpawnerSystemsJS);
+    scheduler.schedule(ut.HTML.InputHandler);
+    scheduler.schedule(ut.HTML.AssetLoader);
+    scheduler.schedule(ut.Core2D.SequencePlayerSystem);
+    scheduler.schedule(ut.HitBox2D.HitBox2DSystem);
+    scheduler.schedule(ut.Shared.InputFence);
+    scheduler.schedule(ut.Shared.UserCodeStart);
+    scheduler.schedule(ut.Shared.UserCodeEnd);
+    scheduler.schedule(ut.UILayout.UICanvasSystem);
+    scheduler.schedule(ut.UILayout.UILayoutSystem);
+    scheduler.schedule(ut.UILayout.SetSprite2DSizeSystem);
+    scheduler.schedule(ut.UILayout.SetRectTransformSizeSystem);
+    scheduler.schedule(ut.HTML.TextMeasurement);
+    scheduler.schedule(ut.Shared.RenderingFence);
+    scheduler.schedule(ut.Core2D.UpdateLocalTransformSystem);
+    scheduler.schedule(ut.Core2D.UpdateWorldTransformSystem);
+    scheduler.schedule(ut.Core2D.Sprite2DInitSystem);
+    scheduler.schedule(ut.Text.Text2DInitSystem);
+    scheduler.schedule(ut.Core2D.DisplayList);
+    scheduler.schedule(ut.Shared.PlatformRenderingFence);
+    scheduler.schedule(ut.Rendering.RendererCanvas);
+    scheduler.schedule(ut.Rendering.RendererGLWebGL);
+    scheduler.schedule(ut.Physics2D.Physics2DSystem);
+
+    // Initialize all configuration data
+    var c0 = world.getConfigData(ut.Core2D.DisplayInfo);
+    c0.width = 800;
+    c0.height = 640;
+    c0.autoSizeToFrame = true;
+    c0.renderMode = 0;
+    world.setConfigData(c0);
+    var c1 = world.getConfigData(ut.Physics2D.Physics2DConfig);
+    var s0 = new ut.Math.Vector2();
+    s0.x = 0;
+    s0.y = -10;
+    c1.gravity = s0;
+    world.setConfigData(c1);
+
+    // Create and initialize all resource entities
+    UT_ASSETS_SETUP(world);
+
+    // Create and initialize all startup entities
+    ut.EntityGroup.instantiate(world, "game.MainScene");
+
+    // Set up the WebSocket client
+    ut._wsclient = ut._wsclient || {};
+    ut._wsclient.world = world;
+
+    // Start the player loop
+    try { ut.Runtime.Service.run(world); } catch (e) { if (e !== 'SimulateInfiniteLoop') throw e; }
+}
